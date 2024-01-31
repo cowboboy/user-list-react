5b4b3dadd8b43cda6e6eb5593d2b53d37afe938e
cowboboy <r2d2tv@yandex.ru>
2024-01-31 20:42:01 +0500

    make resizable columns

--- src/components/user-list/user-list.jsx
+++ src/components/user-list/user-list.jsx
@@ -1,12 +1,59 @@
 import { observer } from 'mobx-react-lite'
 import style from './user-list.module.css'
-import { useContext } from 'react'
+import { useContext, useState, useRef } from 'react'
 import { LOAD_ERROR, LOAD_PENDING, LOAD_DONE } from '../../stores/user-store'
 import { StoreContext } from '../../main'
 import { MyFilterSelect } from '../UI/MySelect/my-select'
 
+const columns = [
+    {
+        text: "ФИО",
+        sortProp: "fio"
+    },
+    {
+        text: "Возраст",
+        sortProp: "age"
+    },
+    {
+        text: "Пол",
+        sortProp: "gender"
+    },
+    {
+        text: "Номер телефона",
+        sortProp: ""
+    },
+    {
+        text: "Адрес",
+        sortProp: "address"
+    }
+]
+
 export const UserList = observer(({showUser}) => {
     const {users} = useContext(StoreContext)
+
+    const [columnWidths, setColumnWidths] = useState({});
+    const tableRef = useRef();
+
+    const handleResize = (columnKey, width) => {
+        setColumnWidths({ ...columnWidths, [columnKey]: width });
+    };
+
+    function handleMouseDown(e, column) {
+        const startX = e.pageX;
+        const startWidth = e.target.getBoundingClientRect().width;
+        const handleMouseMove = moveEvent => {
+            const newWidth =startWidth + moveEvent.pageX - startX;
+            handleResize(column.text, newWidth);
+        };
+
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);                                        
+    }
 
     function renderSwitch(condition) {
         switch(condition) {
@@ -14,34 +61,27 @@ export const UserList = observer(({showUser}) => {
                 return <h1 className={style.pending}>Загрузка...</h1>
             case LOAD_DONE:
                 return (
-                    <table>
+                    <table ref={tableRef}>
                         <thead>
                             <tr>
-                                <th>
-                                    <span>
-                                        ФИО
-                                    </span>
-                                    <MyFilterSelect onChange={e => users.sortByProp("fio", e.target.value)}/>
-                                </th>
-                                <th>
-                                    <span>
-                                        Возраст
-                                    </span>
-                                    <MyFilterSelect onChange={e => users.sortByProp("age", e.target.value)}/>
-                                </th>
-                                <th>
-                                    <span>
-                                        Пол
-                                    </span>
-                                    <MyFilterSelect onChange={e => users.sortByProp("gender", e.target.value)}/>
-                                </th>
-                                <th>Номер телефона</th>
-                                <th>
-                                    <span>
-                                        Адрес
-                                    </span>
-                                    <MyFilterSelect onChange={e => users.sortByProp("fullAddress", e.target.value)}/>
-                                </th>
+                                {
+                                    columns.map(column => (
+                                        <th
+                                            style={{
+                                                minWidth: "50px",
+                                                width: columnWidths[column.text] || 'auto'
+                                            }}
+                                            onMouseDown={e => handleMouseDown(e, column)}
+                                            key={column.text}
+                                        >
+                                            <span>
+                                                {column.text}
+                                            </span>
+                                            <MyFilterSelect onChange={e => users.sortByProp(column.sortProp, e.target.value)}/>
+                                        </th>
+                                    ))
+                                }
+                                
                             </tr>
                         </thead>
                         <tbody>
